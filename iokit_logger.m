a487ef70d023159cea4cff6f006477f1c761b408
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2026-06-29 23:05:34 +0300

    Update iokit_logger.m

--- iokit_logger.m
+++ iokit_logger.m
@@ -5,9 +5,11 @@
 
 typedef unsigned int io_registry_entry_t;
 typedef unsigned int IOOptionBits;
+typedef int kern_return_t;
 
 static NSMutableString *gLogBuffer;
 static UITextView *gLogView;
+static UIWindow *gWindow;
 
 static void appendLog(NSString *line) {
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -18,34 +20,80 @@ static void appendLog(NSString *line) {
     });
 }
 
-// --- IOKit hook ---
+#pragma mark - IOKit hooks
+
+// 1) IORegistryEntryCreateCFProperty (tekil anahtar)
 static CFTypeRef (*orig_IORegistryEntryCreateCFProperty)(
     io_registry_entry_t, CFStringRef, CFAllocatorRef, IOOptionBits);
 
 static CFTypeRef my_IORegistryEntryCreateCFProperty(
     io_registry_entry_t entry, CFStringRef key,
     CFAllocatorRef allocator, IOOptionBits options) {
-
     CFTypeRef result = orig_IORegistryEntryCreateCFProperty(entry, key, allocator, options);
-
     NSString *k = key ? (__bridge NSString *)key : @"(null)";
     NSString *v = @"(null)";
     if (result && CFGetTypeID(result) == CFStringGetTypeID())
         v = (__bridge NSString *)result;
     else if (result)
         v = @"<non-string CFType>";
-
-    appendLog([NSString stringWithFormat:@"%@ = %@", k, v]);
+    appendLog([NSString stringWithFormat:@"[Create] %@ = %@", k, v]);
     return result;
 }
 
-// --- In-app overlay ---
+// 2) IORegistryEntrySearchCFProperty (plane üzerinde arama)
+static CFTypeRef (*orig_IORegistryEntrySearchCFProperty)(
+    io_registry_entry_t, const char *, CFStringRef, CFAllocatorRef, IOOptionBits);
+
+static CFTypeRef my_IORegistryEntrySearchCFProperty(
+    io_registry_entry_t entry, const char *plane, CFStringRef key,
+    CFAllocatorRef allocator, IOOptionBits options) {
+    CFTypeRef result = orig_IORegistryEntrySearchCFProperty(entry, plane, key, allocator, options);
+    NSString *k = key ? (__bridge NSString *)key : @"(null)";
+    NSString *v = @"(null)";
+    if (result && CFGetTypeID(result) == CFStringGetTypeID())
+        v = (__bridge NSString *)result;
+    else if (result)
+        v = @"<non-string CFType>";
+    appendLog([NSString stringWithFormat:@"[Search] %@ = %@", k, v]);
+    return result;
+}
+
+// 3) IORegistryEntryCreateCFProperties (tüm sözlük)
+static kern_return_t (*orig_IORegistryEntryCreateCFProperties)(
+    io_registry_entry_t, CFMutableDictionaryRef *, CFAllocatorRef, IOOptionBits);
+
+static kern_return_t my_IORegistryEntryCreateCFProperties(
+    io_registry_entry_t entry, CFMutableDictionaryRef *props,
+    CFAllocatorRef allocator, IOOptionBits options) {
+    kern_return_t r = orig_IORegistryEntryCreateCFProperties(entry, props, allocator, options);
+    if (r == 0 && props && *props) {
+        for (NSString *key in @[@"IOPlatformSerialNumber", @"IOPlatformUUID"]) {
+            CFTypeRef val = CFDictionaryGetValue(*props, (__bridge CFStringRef)key);
+            if (val && CFGetTypeID(val) == CFStringGetTypeID())
+                appendLog([NSString stringWithFormat:@"[Props] %@ = %@",
+                    key, (__bridge NSString *)val]);
+        }
+    }
+    return r;
+}
+
+#pragma mark - Passthrough window (dokunuşları alttaki uygulamaya geçirir)
+
+@interface PassthroughWindow : UIWindow @end
+@implementation PassthroughWindow
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    UIView *hit = [super hitTest:point withEvent:event];
+    if (hit == self.rootViewController.view) return nil; // boş alan -> alttaki uygulamaya geç
+    return hit;
+}
+@end
+
+#pragma mark - Overlay UI
+
 @interface IOKitLogOverlay : NSObject
 + (void)copyTapped;
 + (void)toggleTapped;
 @end
-
-static UIWindow *gWindow;
 
 @implementation IOKitLogOverlay
 + (void)copyTapped {
@@ -66,15 +114,13 @@ static void setupOverlay(void) {
     }
     if (!scene) return;
 
-    gWindow = [[UIWindow alloc] initWithWindowScene:scene];
+    gWindow = [[PassthroughWindow alloc] initWithWindowScene:scene];
     gWindow.windowLevel = UIWindowLevelAlert + 1;
     gWindow.backgroundColor = UIColor.clearColor;
     gWindow.frame = scene.coordinateSpace.bounds;
 
     UIViewController *vc = [UIViewController new];
     gWindow.rootViewController = vc;
-    // overlay'in arkasındaki uygulamaya dokunmaları geçirmek için:
-    vc.view.userInteractionEnabled = YES;
     vc.view.backgroundColor = UIColor.clearColor;
 
     CGRect b = vc.view.bounds;
@@ -83,24 +129,15 @@ static void setupOverlay(void) {
     gLogView = [[UITextView alloc] initWithFrame:
         CGRectMake(0, b.size.height - h, b.size.width, h)];
     gLogView.editable = NO;
-    gLogView.selectable = YES;            // uzun bas -> kopyala
+    gLogView.selectable = YES;
     gLogView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.8];
     gLogView.textColor = UIColor.greenColor;
     gLogView.font = [UIFont fontWithName:@"Menlo" size:11];
     gLogView.text = gLogBuffer ?: @"[IOKit] hazır\n";
     [vc.view addSubview:gLogView];
 
-    UIButton *copyBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    copyBtn.frame = CGRectMake(b.size.width - 80, b.size.height - h - 36, 70, 30);
-    [copyBtn setTitle:@"Kopyala" forState:UIControlStateNormal];
-    copyBtn.backgroundColor = [UIColor colorWithWhite:0 alpha:0.8];
-    [copyBtn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
-    [copyBtn addTarget:IOKitLogOverlay.class action:@selector(copyTapped)
-        forControlEvents:UIControlEventTouchUpInside];
-    [vc.view addSubview:copyBtn];
-
     UIButton *toggleBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    toggleBtn.frame = CGRectMake(10, b.size.height - h - 36, 70, 30);
+    toggleBtn.frame = CGRectMake(10, b.size.height - h - 36, 80, 30);
     [toggleBtn setTitle:@"Gizle/Aç" forState:UIControlStateNormal];
     toggleBtn.backgroundColor = [UIColor colorWithWhite:0 alpha:0.8];
     [toggleBtn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
@@ -108,18 +145,34 @@ static void setupOverlay(void) {
         forControlEvents:UIControlEventTouchUpInside];
     [vc.view addSubview:toggleBtn];
 
+    UIButton *copyBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    copyBtn.frame = CGRectMake(b.size.width - 90, b.size.height - h - 36, 80, 30);
+    [copyBtn setTitle:@"Kopyala" forState:UIControlStateNormal];
+    copyBtn.backgroundColor = [UIColor colorWithWhite:0 alpha:0.8];
+    [copyBtn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+    [copyBtn addTarget:IOKitLogOverlay.class action:@selector(copyTapped)
+        forControlEvents:UIControlEventTouchUpInside];
+    [vc.view addSubview:copyBtn];
+
     gWindow.hidden = NO;
 }
 
+#pragma mark - Constructor
+
 __attribute__((constructor))
 static void init_logger(void) {
-    rebind_symbols((struct rebinding[1]){
+    rebind_symbols((struct rebinding[3]){
         {"IORegistryEntryCreateCFProperty",
          my_IORegistryEntryCreateCFProperty,
-         (void *)&orig_IORegistryEntryCreateCFProperty}
-    }, 1);
+         (void *)&orig_IORegistryEntryCreateCFProperty},
+        {"IORegistryEntrySearchCFProperty",
+         my_IORegistryEntrySearchCFProperty,
+         (void *)&orig_IORegistryEntrySearchCFProperty},
+        {"IORegistryEntryCreateCFProperties",
+         my_IORegistryEntryCreateCFProperties,
+         (void *)&orig_IORegistryEntryCreateCFProperties}
+    }, 3);
 
-    // UI hazır olduğunda overlay'i kur (uygulama tam açılmadan scene olmayabilir)
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC),
                    dispatch_get_main_queue(), ^{ setupOverlay(); });
 }
