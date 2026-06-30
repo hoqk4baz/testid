eb98a95b80c6334d9a613361fa8978e65ea56662
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2026-06-30 16:16:34 +0300

    Update iokit_logger.m

--- iokit_logger.m
+++ iokit_logger.m
@@ -1,206 +1,77 @@
-// iokit_logger.m
 #import <UIKit/UIKit.h>
-#import <CoreFoundation/CoreFoundation.h>
 #import <Security/Security.h>
-#import <objc/runtime.h>
-#import "fishhook.h"
 
-typedef int kern_return_t;
+static UITextView *logView;
+static NSMutableString *logs;
 
-static NSMutableString *gLogBuffer;
-static UITextView *gLogView;
-static UIWindow *gWindow;
-
-static NSString * const kTargetAccount = @"lm_new_device_deviceIdentifier";
-
-static void appendLog(NSString *line) {
+void AddLog(NSString *text) {
+    NSString *line = [NSString stringWithFormat:@"[%@] %@\n",
+                      [NSDate date], text];
+    [logs appendString:line];
     dispatch_async(dispatch_get_main_queue(), ^{
-        if (!gLogBuffer) gLogBuffer = [NSMutableString string];
-        [gLogBuffer appendString:line];
-        [gLogBuffer appendString:@"\n"];
-        gLogView.text = gLogBuffer;
+        logView.text = logs;
     });
 }
 
-// NSData -> okunabilir döküm. bplist ise decode eder.
-static NSString *describeData(NSData *d) {
-    if (!d) return @"(no-data)";
+void ClearKeychain(void) {
+    NSDictionary *query = @{(__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword};
+    OSStatus status = SecItemDelete((__bridge CFDictionaryRef)query);
 
-    if (d.length >= 8) {
-        const unsigned char *b = d.bytes;
-        if (b[0]==0x62 && b[1]==0x70 && b[2]==0x6c && b[3]==0x69 &&
-            b[4]==0x73 && b[5]==0x74) {                       // "bplist"
-            NSError *err = nil;
-            id plist = [NSPropertyListSerialization
-                propertyListWithData:d options:NSPropertyListImmutable
-                              format:NULL error:&err];
-            if (plist) {
-                NSString *desc = [plist description];
-                NSString *un = @"";
-                @try {
-                    id obj = [NSKeyedUnarchiver unarchiveObjectWithData:d];
-                    if (obj) un = [NSString stringWithFormat:@"\n  unarchived=%@", obj];
-                } @catch (__unused NSException *e) {}
-                return [NSString stringWithFormat:@" bplist len=%lu\n  plist=%@%@",
-                        (unsigned long)d.length, desc, un];
-            }
-        }
-    }
-
-    NSString *utf8 = [[NSString alloc] initWithData:d encoding:NSUTF8StringEncoding];
-    if (utf8) return [NSString stringWithFormat:@" utf8=%@", utf8];
-
-    const unsigned char *bb = d.bytes;
-    NSMutableString *hex = [NSMutableString string];
-    for (NSUInteger i = 0; i < d.length; i++) [hex appendFormat:@"%02x", bb[i]];
-    NSString *b64 = [d base64EncodedStringWithOptions:0];
-    return [NSString stringWithFormat:@" len=%lu\n  hex=%@\n  b64=%@",
-            (unsigned long)d.length, hex, b64];
+    AddLog([NSString stringWithFormat:@"Keychain cleared (%d)", (int)status]);
 }
 
-#pragma mark - keychain hooks (sadece hedef anahtar)
-
-static OSStatus (*orig_SecItemCopyMatching)(CFDictionaryRef, CFTypeRef *);
-static OSStatus my_SecItemCopyMatching(CFDictionaryRef query, CFTypeRef *result) {
-    OSStatus s = orig_SecItemCopyMatching(query, result);
-    CFTypeRef acct = query ? CFDictionaryGetValue(query, kSecAttrAccount) : NULL;
-    if (acct && CFGetTypeID(acct) == CFStringGetTypeID()) {
-        NSString *a = (__bridge NSString *)acct;
-        if ([a isEqualToString:kTargetAccount]) {
-            NSString *val = @" (value döndürülmedi)";
-            if (s == 0 && result && *result &&
-                CFGetTypeID(*result) == CFDataGetTypeID()) {
-                val = describeData((__bridge NSData *)*result);
-            }
-            appendLog([NSString stringWithFormat:@"[KC-read] %@ status=%d%@", a, (int)s, val]);
-        }
-    }
-    return s;
-}
-
-static OSStatus (*orig_SecItemAdd)(CFDictionaryRef, CFTypeRef *);
-static OSStatus my_SecItemAdd(CFDictionaryRef attributes, CFTypeRef *result) {
-    OSStatus s = orig_SecItemAdd(attributes, result);
-    CFTypeRef acct = attributes ? CFDictionaryGetValue(attributes, kSecAttrAccount) : NULL;
-    if (acct && CFGetTypeID(acct) == CFStringGetTypeID()) {
-        NSString *a = (__bridge NSString *)acct;
-        if ([a isEqualToString:kTargetAccount]) {
-            CFTypeRef data = CFDictionaryGetValue(attributes, kSecValueData);
-            NSString *val = (data && CFGetTypeID(data) == CFDataGetTypeID())
-                ? describeData((__bridge NSData *)data) : @" (no-data)";
-            appendLog([NSString stringWithFormat:@"[KC-WRITE] %@ status=%d%@", a, (int)s, val]);
-        }
-    }
-    return s;
-}
-
-static OSStatus (*orig_SecItemUpdate)(CFDictionaryRef, CFDictionaryRef);
-static OSStatus my_SecItemUpdate(CFDictionaryRef query, CFDictionaryRef attributesToUpdate) {
-    OSStatus s = orig_SecItemUpdate(query, attributesToUpdate);
-    CFTypeRef acct = query ? CFDictionaryGetValue(query, kSecAttrAccount) : NULL;
-    if (acct && CFGetTypeID(acct) == CFStringGetTypeID()) {
-        NSString *a = (__bridge NSString *)acct;
-        if ([a isEqualToString:kTargetAccount]) {
-            CFTypeRef data = CFDictionaryGetValue(attributesToUpdate, kSecValueData);
-            NSString *val = (data && CFGetTypeID(data) == CFDataGetTypeID())
-                ? describeData((__bridge NSData *)data) : @" (no-data)";
-            appendLog([NSString stringWithFormat:@"[KC-UPDATE] %@ status=%d%@", a, (int)s, val]);
-        }
-    }
-    return s;
-}
-
-#pragma mark - Passthrough window
-
-@interface PassthroughWindow : UIWindow @end
-@implementation PassthroughWindow
-- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
-    UIView *hit = [super hitTest:point withEvent:event];
-    if (hit == self.rootViewController.view) return nil;
-    return hit;
-}
+@interface DebugWindow : UIWindow
 @end
 
-#pragma mark - Overlay UI
+@implementation DebugWindow
+- (instancetype)init {
+    self = [super initWithFrame:[UIScreen mainScreen].bounds];
+    if (self) {
+        logs = [NSMutableString new];
 
-@interface IOKitLogOverlay : NSObject
-+ (void)copyTapped;
-+ (void)toggleTapped;
-@end
-@implementation IOKitLogOverlay
-+ (void)copyTapped { [UIPasteboard generalPasteboard].string = gLogBuffer ?: @""; }
-+ (void)toggleTapped { gLogView.hidden = !gLogView.hidden; }
-@end
+        UIViewController *vc = [UIViewController new];
+        self.rootViewController = vc;
+        self.windowLevel = UIWindowLevelAlert + 1;
+        [self makeKeyAndVisible];
 
-static void setupOverlay(void) {
-    UIWindowScene *scene = nil;
-    for (UIScene *s in UIApplication.sharedApplication.connectedScenes) {
-        if ([s isKindOfClass:UIWindowScene.class] &&
-            s.activationState == UISceneActivationStateForegroundActive) {
-            scene = (UIWindowScene *)s; break;
-        }
+        logView = [[UITextView alloc] initWithFrame:CGRectMake(10, 50,
+                                self.frame.size.width - 20,
+                                self.frame.size.height - 100)];
+        logView.editable = NO;
+        [vc.view addSubview:logView];
+
+        UIButton *copy = [UIButton buttonWithType:UIButtonTypeSystem];
+        copy.frame = CGRectMake(10, 10, 80, 30);
+        [copy setTitle:@"Copy" forState:UIControlStateNormal];
+        [copy addTarget:self action:@selector(copyLogs) forControlEvents:UIControlEventTouchUpInside];
+        [vc.view addSubview:copy];
+
+        UIButton *clear = [UIButton buttonWithType:UIButtonTypeSystem];
+        clear.frame = CGRectMake(100, 10, 80, 30);
+        [clear setTitle:@"Clear" forState:UIControlStateNormal];
+        [clear addTarget:self action:@selector(clearLogs) forControlEvents:UIControlEventTouchUpInside];
+        [vc.view addSubview:clear];
+
+        ClearKeychain();
+        AddLog(@"App started");
     }
-    if (!scene) return;
-
-    gWindow = [[PassthroughWindow alloc] initWithWindowScene:scene];
-    gWindow.windowLevel = UIWindowLevelAlert + 1;
-    gWindow.backgroundColor = UIColor.clearColor;
-    gWindow.frame = scene.coordinateSpace.bounds;
-
-    UIViewController *vc = [UIViewController new];
-    gWindow.rootViewController = vc;
-    vc.view.backgroundColor = UIColor.clearColor;
-
-    CGRect b = vc.view.bounds;
-    CGFloat h = b.size.height * 0.40;
-
-    gLogView = [[UITextView alloc] initWithFrame:
-        CGRectMake(0, b.size.height - h, b.size.width, h)];
-    gLogView.editable = NO;
-    gLogView.selectable = YES;
-    gLogView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.85];
-    gLogView.textColor = UIColor.greenColor;
-    gLogView.font = [UIFont fontWithName:@"Menlo" size:10];
-    gLogView.text = gLogBuffer ?: @"[hazır] lm_new_device_deviceIdentifier izleniyor\n";
-    [vc.view addSubview:gLogView];
-
-    UIButton *toggleBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    toggleBtn.frame = CGRectMake(10, b.size.height - h - 36, 80, 30);
-    [toggleBtn setTitle:@"Gizle/Aç" forState:UIControlStateNormal];
-    toggleBtn.backgroundColor = [UIColor colorWithWhite:0 alpha:0.85];
-    [toggleBtn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
-    [toggleBtn addTarget:IOKitLogOverlay.class action:@selector(toggleTapped)
-        forControlEvents:UIControlEventTouchUpInside];
-    [vc.view addSubview:toggleBtn];
-
-    UIButton *copyBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    copyBtn.frame = CGRectMake(b.size.width - 90, b.size.height - h - 36, 80, 30);
-    [copyBtn setTitle:@"Kopyala" forState:UIControlStateNormal];
-    copyBtn.backgroundColor = [UIColor colorWithWhite:0 alpha:0.85];
-    [copyBtn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
-    [copyBtn addTarget:IOKitLogOverlay.class action:@selector(copyTapped)
-        forControlEvents:UIControlEventTouchUpInside];
-    [vc.view addSubview:copyBtn];
-
-    gWindow.hidden = NO;
+    return self;
 }
 
-#pragma mark - Constructor
+- (void)copyLogs {
+    UIPasteboard.generalPasteboard.string = logs;
+    AddLog(@"Logs copied");
+}
+
+- (void)clearLogs {
+    [logs setString:@""];
+    logView.text = @"";
+}
+@end
 
 __attribute__((constructor))
-static void init_logger(void) {
-    rebind_symbols((struct rebinding[3]){
-        {"SecItemCopyMatching",
-         my_SecItemCopyMatching,
-         (void *)&orig_SecItemCopyMatching},
-        {"SecItemAdd",
-         my_SecItemAdd,
-         (void *)&orig_SecItemAdd},
-        {"SecItemUpdate",
-         my_SecItemUpdate,
-         (void *)&orig_SecItemUpdate}
-    }, 3);
-
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC),
-                   dispatch_get_main_queue(), ^{ setupOverlay(); });
+static void init() {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[DebugWindow alloc] init];
+    });
 }
